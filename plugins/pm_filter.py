09e6c99d47c8e0414d83fb1d0443aebaf8f86372
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-02-06 22:58:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -151,7 +151,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     elif query.data == 'dsclose':
         if query.from_user.id == query.message.reply_to_message.from_user.id:
-            await query.answer(Jake Khud Ka Search Kr Dusre ka delete mat kr)
+            await query.answer("Jake Khud Ka Search Kr Dusre ka delete mat kr",show_alert=True)
             await query.message.delete()
             await query.message.reply_to_message.delete()
         else:
