2cd8198b4aa8e734c531eb38ef2ffe2cf4124db4
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-02-06 23:09:12 +0530

    Error

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("Jake Khud Ka Search Kr Dusre ka delete mat kr",show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -151,7 +151,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.delete()
     elif query.data == 'dsclose':
         if query.from_user.id == query.message.reply_to_message.from_user.id:
-            await query.answer("Jake Khud Ka Search Kr Dusre ka delete mat kr",show_alert=True)
+            await query.answer(Done ✅",show_alert=True)
             await query.message.delete()
             await query.message.reply_to_message.delete()
         else:
